1b0ee6fd53091868e6b091b0aa26699daf703a99
James <sillybillypiggy@gmail.com>
2016-01-16 21:37:29 +0000

    Migrate mention.py

--- mention.py
+++ mention.py
@@ -1,4 +1,11 @@
 class mention:
 	def main():
-	
+                members = client.get_all_members()
+                users = []
+                toMessage = "Mentioning all members in " + args[1] + " : "
+                for member in members:
+                    print(member.name)
+                    if listContains(member.roles, args[1].lower()):
+                        toMessage = toMessage + " " + member.mention()
+                client.send_message(message.channel, toMessage)
 	def help():
