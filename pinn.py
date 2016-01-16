930fc6d48fa1f17cbd114540303201c3ee24d782
Sam Gunner <samgunnerpc@live.com>
2016-01-16 22:48:05 +0000

    Added module 'pinn'

--- pinn.py
+++ pinn.py
@@ -2,9 +2,14 @@
 
 import random
 pinnLinks = [
-	"",
+	"http://i.imgur.com/4uBVAlv.jpg",
+	"http://i.imgur.com/N7ZLdtD.jpg",
+	"http://i.imgur.com/aTDVeFX.jpg",
 ]
 
 def main(message, comms, client):
-	ranNum = random.randint(1, 3)
+	ranNum = random.randint(0, 2)
+	client.send_message(message.channel, pinnLinks[ranNum])
 	
+def help(message, comms, client):
+	client.send_message(message.channel, "Posts a random picture of you-know-who.")
