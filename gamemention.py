a07a1f40809efb3096aa4597962ae3ce05e7df6f
Sam Gunner <samgunnerpc@live.com>
2016-01-16 21:54:19 +0000

    .

--- gamemention.py
+++ gamemention.py
@@ -2,18 +2,15 @@ import discord
 import sys
 import hello
 import lenny
-import help
 
 client = discord.Client()
 client.login("james@hurcomb.net", "ALongerPassword")
 
 def runCommand(commTbl, message):
     if (commTbl[0] in sys.modules):
-        try:
-            prog = getattr(sys.modules, commTbl[0])
-            prog.main(message, commTbl)
-        except AttributeError:
-            log("Fuck")
+        print("hey!")
+        prog = sys.modules[commTbl[0]]
+        prog.main(message, commTbl, client)
 
 def listContains(listMain, test):
     for a in listMain:
@@ -26,7 +23,7 @@ def log(text):
 @client.event
 def on_message(message):
     if message.content.startswith("!"):
-        comm = message[1:]
+        comm = message.content[1:]
         comms = comm.split(" ")
         log("Attempting to run command: " + comms[0])
         runCommand(comms, message)
