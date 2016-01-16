43487a376d02851c7c1a7290cb6b93f1f5b6bfc3
James <sillybillypiggy@gmail.com>
2016-01-16 22:12:41 +0000

    add some help

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def main(t, t1, client):
         client.send_message(t.channel, "Hello, world!")
-def help():
-        print("f")
+def help(message, args, client):
+        print("A nerdier ping. Usage: !hello")
--- lenny.py
+++ lenny.py
@@ -1,4 +1,4 @@
 def main(t, t1, client):
         client.send_message(t.channel, "( ͡° ͜ʖ ͡°)")
-def help():
-        print("f")
+def help(message, args, client):
+        print("Inserts a lenny face. An all purpose command for all situations. Usage: !lenny")
