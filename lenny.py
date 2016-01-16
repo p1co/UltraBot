7c724452757873f704244ed1b5186b3dd042e84b
James <james@hurcombl.net>
2016-01-16 22:14:51 +0000

    Sendmessage

--- lenny.py
+++ lenny.py
@@ -1,4 +1,4 @@
 def main(t, t1, client):
         client.send_message(t.channel, "( ͡° ͜ʖ ͡°)")
 def help(message, args, client):
-        print("Inserts a lenny face. An all purpose command for all situations. Usage: !lenny")
+        client.send_message(message.channel,"Inserts a lenny face. An all purpose command for all situations. Usage: !lenny")
