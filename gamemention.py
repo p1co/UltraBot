48c11b5188caf0495923a874e480d4ebfb79a057
Sam Gunner <samgunnerpc@live.com>
2016-01-16 18:41:50 +0000

    Redone - Added modular command structure

--- gamemention.py
+++ gamemention.py
@@ -8,19 +8,33 @@ def listContains(listMain, test):
         if a.name == test:
             return True
 
+
+class newCommands:
+    def mention(message, args):
+        if message.content.startswith('!mention'):
+            members = client.get_all_members()
+            toSend = []
+            users = []
+            toMessage = ""
+            for member in members:
+                if listContains(member.roles, args[1].lower()):
+                    toMessage = toMessage + " " + member.mention()
+        client.send_message(message.channel, toMessage)
+
+
+
+commands_main = newCommands()
+
 @client.event
 def on_message(message):
-    
-    if message.content.startswith('!GTA'):
-        members = client.get_all_members()
-        toSend = []
-        users = []
-        toMessage = ""
-        for member in members:
-            if listContains(member.roles, "gta"):
-                toMessage = toMessage + " " + member.mention()
-        client.send_message(message.channel, toMessage, mentions=True)
-            
+    messCont = message.content[1:]
+    commTbl = messCont.split(" ")
+    try:
+        toCall = getattr(newCommands, commTbl[0])
+        toCall(message, commTbl)
+    except AttributeError:
+        print("fuck")
+
 @client.event
 def on_ready():
     print('Logged in as')
