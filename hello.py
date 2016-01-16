4e866e2e1c5e00dc3c606599855da8487430c949
James <james@hurcombl.net>
2016-01-16 22:15:06 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def main(t, t1, client):
         client.send_message(t.channel, "Hello, world!")
 def help(message, args, client):
-        print("A nerdier ping. Usage: !hello")
+        client.send_message(message.channel,"A nerdier ping. Usage: !hello")
