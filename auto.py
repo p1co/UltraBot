4a4c7ed8d7f687868f3447c8f7eaaafdc0dcc911
James <sillybillypiggy@gmail.com>
2016-01-16 22:40:46 +0000

    angreir when fighting and u/ur

--- auto.py
+++ auto.py
@@ -12,8 +12,14 @@ def main(t, client):
         elif "definately" in str.lower() or "definetly" in str.lower():
             client.send_message(t.channel, "You mean DEFINITELY")
             return True
+        elif " ur " in str.lower():
+            client.send_message(t.channel, "You mean YOUR/YOU'RE")
+            return True
+        elif " u " in str.lower():
+            client.send_message(t.channel, "You mean YOU")
+            return True
         elif "u wot" in str.lower() or "fight me" in str.lower() or "fite me" in str.lower():
             client.send_message(t.channel, "(╯°□°)╯︵ ┻━┻")
-            client.send_message(t.channel, "Don't pick on my friends")
+            client.send_message(t.channel, "DON'T PICK ON MY FRIENDS")
             client.send_message(t.channel, "You've made a very powerful enemy")
             return True
