0bfad85c807b15bee061d30377ba92a9fc0ce56c
TheLadInAPooTin <elliotwilson@rocketmail.com>
2016-01-18 17:40:50 +0000

    Added !definitely
    Add this because i saw someone misspell it, and also i wanted to try
    adding something

---
+++ modules/definitely.py
@@ -0,0 +1,8 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #definitelylink-generating module for the bot.
+
+def main(t, t1, client):
+        client.send_message(t.channel, "http://www.d-e-f-i-n-i-t-e-l-y.com/")
+def help(message, args, client):
+        client.send_message(message.channel,"Someone spelt it wrong again? Usage: !definitely")
