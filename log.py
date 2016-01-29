c8c4d1d023f2547586bf6e61f9380c1b7d30577c
Sam Gunner <samgunnerpc@live.com>
2016-01-29 19:49:29 +0000

    Delete log.py

--- log.py
+++
@@ -1,6 +0,0 @@
-def log(text): # Simple console-logging function.
-    print("[INFO] " + text)
-def warn(text): # Simple console-logging function.
-    print("[WARNING] " + text)
-def error(text): # Simple console-logging function.
-    print("[ERROR] " + text)
