e5c52bd82551acb3fe8acc0997f3bdc9f08f254e
James <sillybillypiggy@gmail.com>
2016-01-30 22:12:23 +0000

    tell console if command is not found
    Useful for debugging or something

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ async def runCommand(commTbl, message, moduleList): # Define main command-proces
         except Exception as ex:
             log("Error occured in " + commTbl[0] + ": " + str(ex), level=3)
     else: # If the command couldn't be found, display error message.
+        log("The command: " + commTbl[0] + " was not found.", level=2)
         await client.send_message(message.channel, "The command specified could not be found, " + message.author.mention +".")
     
 
