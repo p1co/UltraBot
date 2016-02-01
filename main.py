2f839a58ae1460ad4801185b917a8a8a0fe751ef
Sam Gunner <samgunnerpc@live.com>
2016-02-01 19:09:17 +0000

    Updated - changed dontLoad to ignore_modules & removed log

--- main.py
+++ main.py
@@ -107,9 +107,7 @@ async def on_message(message): # On message. This tries to figure out if it is a
             else:
                 log("Stopped spam from " + message.author.name, level=2)
         else:
-            if "auto.py" in data["dontload"]:
-                log("Auto was not loaded, not doing anything", level=2)
-            else:
+            if not ("auto.py" in data["ignore_modules"]):
                 await loaded['auto'].main(message, client) # Run auto module on the text that has been sent.
 
 
